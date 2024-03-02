a3ecf3ce760a93a000dd1d955c553483ddb3e77c
Alexandrechs <alexandrechs@hotmail.com>
2024-03-01 21:45:18 -0300

    Adição de feedback visual na execução de rotas de API e nas dependências dos testes E2E (Snapshot Only Mode).

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost',
+    env: {
+      hideCredentials: true,
+      requestMode: true,
+      // snapshotOnly: true, (Será passado direto no teste conforme o curso, mas essa funcionalidade pode ser ativa por aqui também)
+    },
   },
   fixturesFolder: false,
   video: false,
